e12848b6eb1a6b229fe5afc9bb4de525d06fd7f4
Luka Golinar <luka.golinar@gmail.com>
2025-05-13 16:10:55 +0200

    feat: some comments

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,5 +3,7 @@ import { processStakingData } from "./procedures/staking";
 
 export async function processData(blocks: Number[]) {
   const db = await initOrGetDatabase();
+
   await processStakingData(db, blocks);
+  // NOTE: Add more custom procedures here
 }
