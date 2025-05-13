0ad8d6572dc389ed515966232ff063117733976c
Luka Golinar <luka.golinar@gmail.com>
2025-05-13 14:50:54 +0200

    data procedures (#4)

---
+++ src/db/typeormDataSource.ts
@@ -0,0 +1,25 @@
+import { DataSource } from "typeorm";
+import { CollectedReward } from "@/model/staking/CollectedRewards";
+import { EstimatedReward } from "@/model/staking/EstimatedRewards";
+import { NodeProofRate } from "@/model/staking/NodeProofRate";
+
+let AppDataSource: DataSource;
+
+export function initDatabase() {
+  AppDataSource = new DataSource({
+    type: "postgres",
+    host: process.env.PG_HOST,
+    port: parseInt(process.env.DB_PORT) || 5432,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [CollectedReward, EstimatedReward, NodeProofRate],
+    // This means we always update the database schema to match the entities
+    synchronize: true,
+    schema: process.env.DB_SCHEMA || "public",
+  });
+
+  AppDataSource.initialize().catch((error) => console.log(error));
+  console.log("[✔] Type ORM Data Source initialized. Using schema:", process.env.DB_SCHEMA);
+  return AppDataSource;
+}
